b2279cc2713b8e941ce42fa30b7c7a3b607b4eac
sigma21 <semihkocakel@hotmail.com>
2020-01-04 19:51:19 +0100

    deployment settings

--- blog/settings.py
+++ blog/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's9o8)w&+lsl-v@m5-i67#3kmmocr)d4t557!r23jf1crgct-ny'
+# SECRET_KEY = 's9o8)w&+lsl-v@m5-i67#3kmmocr)d4t557!r23jf1crgct-ny'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
